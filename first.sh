497a1dbae1e0752ff5724ca9b44704f25d22123d
Keegan Bowen <1106191+jpegleg@users.noreply.github.com>
2024-09-24 00:51:59 -0400

    Update first.sh
    add apk-tools and adjust line style

--- first.sh
+++ first.sh
@@ -1,4 +1,8 @@
 #!/bin/ash
 apk update
 apk upgrade
-apk add git vim tmux wget curl python3 perl cmake gcc clang openssl htop bash fish alpine-sdk
+# alpine linux packages for making alpine packages
+# plus some administrative tools such as vim and tmux
+apk add git vim tmux wget curl python3 perl \
+  cmake gcc clang openssl htop bash fish \
+  alpine-sdk apk-tools apk-tools-dev apk-tools-static
