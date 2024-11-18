649d619f3192991139d740ff2b8de2497f55bdac
Keegan Bowen <1106191+jpegleg@users.noreply.github.com>
2024-11-17 19:00:28 -0500

    Update mod.rs
    update comment
    remove unused crate clap

--- winter/src/roots/mod.rs
+++ winter/src/roots/mod.rs
@@ -3,12 +3,17 @@ use std::net::TcpStream;
 use std::path::Path;
 
 use ssh2::Session;
-use clap::Parser;
 use anyhow::{anyhow, Context, Result};
 use shellexpand::tilde;
 use chrono::Utc;
 
-const UNPACK: &str = include_str!("unpack.ash");
-const LETSEN: &str = include_str!("letsen.ash");
-const SSHPRT: &str = include_str!("sshprt.cfg");
-// reactionary SSH functions below...
+const UNPACK: &str = include_str!("../../unpack.ash");
+const LETSEN: &str = include_str!("../../letsen.ash");
+const SSHPRT: &str = include_str!("../../sshprt.cfg");
+
+// ssh function, also see "cone" as related code...
+//  these are loaded from (ASH) script files at compile time:
+// unpack is a restore function: unpack a tarball
+// letsen is a PKI function: use certbot
+// sshprt is setting the SSH port to use
+// 
