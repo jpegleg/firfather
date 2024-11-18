a5ce93242f1379efe2ed24e4ed39e9f5617e48c8
Keegan Bowen <1106191+jpegleg@users.noreply.github.com>
2024-11-17 19:35:07 -0500

    Create main.rs
    example load config structure

---
+++ winter/src/main.rs
@@ -0,0 +1,35 @@
+use std::io::{Read, Write};
+use std::fs::File;
+use std::thread;
+use uuid::Uuid;
+use chrono::prelude::*;
+use serde::Deserialize;
+
+mod roots;
+mod needles;
+
+// targets for winter to manage
+#[derive(Deserialize)]
+struct Config {
+    newconeaddress: String,
+    newconerootport: String,
+    newkeypath: String,
+    currentconeaddress: String,
+    currentconerootport: String,
+    currentkeypath: String,
+}
+
+fn main() -> std::io::Result<()> {
+    let mut file = File::open("/etc/winter.toml")?;
+    let mut contents = String::new();
+    file.read_to_string(&mut contents)?;
+    let config: Config = toml::from_str(&contents).unwrap();
+    let initu = Utc::now();
+    // edit this section to create the logic as to when
+    // to execute reactions
+    //
+    // make decisions then if we need to restore a cone from tarball, for example
+    //     unpackreact(key_path, username, hostname, tcp_address)
+    // where the values passed are populated either from the winter.toml
+    Ok(())
+}
