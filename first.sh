80264a5841a4f56e00b2b5ccfbb0bedb9c09b400
Keegan Bowen <1106191+jpegleg@users.noreply.github.com>
2024-11-24 18:33:28 -0500

    Update first.sh
    add customized motd

--- first.sh
+++ first.sh
@@ -6,3 +6,5 @@ apk upgrade
 apk add git vim tmux wget curl python3 perl \
   cmake gcc clang openssl htop bash fish \
   alpine-sdk apk-tools apk-tools-dev apk-tools-static
+# comment out the following line if a different motd is desired
+grep "Serotinous Cones" /etc/motd || cp ./motd /etc/motd || echo "no motd file found in $(pwd)" # set the motd to the fun firfather motd
